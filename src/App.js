29a4bfd9713dee9fc80b137b42daa8111e202a3b
Sai <sairout92@gmail.com>
2022-12-08 23:36:20 +0530

    api call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
@@ -20,6 +21,8 @@ function App() {
       </header>
     </div>
   );
+
+  
 }
 
 export default App;
---
+++ src/Demo.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+function Demo(){
+    return (
+        <div>
+            <button onClick={getApi} >Click Me</button>
+        </div>
+    );
+
+    function getApi(){
+        axios.get("https://demo-spring.azurewebsites.net/asdasda").then(response => console.log(response));
+      }
+}
+
+export default Demo;
