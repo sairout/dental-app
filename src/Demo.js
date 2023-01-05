89fb2f14e2a2d0e84b077e124beba1a2996c23ee
Sai <sairout92@gmail.com>
2023-01-05 16:18:43 +0530

    updated api call

--- src/Demo.js
+++ src/Demo.js
@@ -3,13 +3,19 @@ import axios from 'axios';
 function Demo(){
     return (
         <div>
-            <button onClick={getApi} >Click Me</button>
+            <button onClick={getSuccessApi} >Okay</button>
+            <button onClick={getFailedApi} >Not Okay</button>
         </div>
+        
     );
 
-    function getApi(){
-        axios.get("https://demo-spring.azurewebsites.net/home").then(response => console.log(response));
-      }
+    function getSuccessApi(){
+        axios.get("https://demo-spring.azurewebsites.net/").then(response => console.log(response));
+    }
+    
+    function getSuccessApi(){
+        axios.get("https://demo-spring.azurewebsites.net/asd").then(response => console.log(response));
+    }
 }
 
 export default Demo;
